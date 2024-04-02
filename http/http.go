50e7256bbb9328ea5e489b6acbbbce7a9a31c506
“mink” <2381385438@qq.com>
2024-04-02 16:57:29 +0800

    advent http method

--- http/http.go
+++ http/http.go
@@ -3,14 +3,21 @@ package http
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"time"
 )
 
+type Method string
+
+const (
+	Get  Method = "GET"
+	Post Method = "POST"
+)
+
 type Param struct {
+	Method   Method
 	Url      string
 	Body     interface{}
 	Auth     string
@@ -19,32 +26,44 @@ type Param struct {
 	Callback func(b []byte) error
 }
 
-func Post(p Param) error {
-	jsonStr, _ := json.Marshal(p.Body)
-	fmt.Printf("[Http Util] post : %s\nbody: %s\nauth:%s\n ", p.Url, jsonStr, p.Auth)
-	req, err := http.NewRequest("POST", p.Url, bytes.NewBuffer(jsonStr))
+func Request(p Param) error {
+	jsonStr, err := json.Marshal(p.Body)
 	if err != nil {
-		fmt.Println("[Http Util] post new request err : ", err)
+		fmt.Println("[Http Util] Marshal body err: ", err)
 		return err
 	}
+
+	fmt.Printf("[Http Util] method: %s, url: %s, body: %s, auth: %s\n", p.Method, p.Url, jsonStr, p.Auth)
+
+	req, err := http.NewRequest(string(p.Method), p.Url, bytes.NewBuffer(jsonStr))
+	if err != nil {
+		fmt.Println("[Http Util] New request err: ", err)
+		return err
+	}
+
 	for k, v := range p.Header {
 		req.Header.Add(k, v)
 	}
+
 	if p.Auth != "" {
 		req.Header.Add("Authorization", p.Auth)
 	}
-	defer req.Body.Close()
-	client := &http.Client{Timeout: time.Duration(p.TimeOut) * time.Second}
+
+	client := &http.Client{
+		Timeout: time.Duration(p.TimeOut) * time.Second,
+	}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("[Http Util] post client err : ", err)
+		fmt.Println("[Http Util] Client err: ", err)
 		return err
 	}
+
+	defer resp.Body.Close()
 	result, _ := io.ReadAll(resp.Body)
 	fmt.Println("[Http Util] post resp: ", string(result))
 	if resp.StatusCode != 200 {
-		return errors.New(string(result))
+		return fmt.Errorf("unexpected response status: %d", resp.StatusCode)
 	}
-	defer resp.Body.Close()
+
 	return p.Callback(result)
 }
